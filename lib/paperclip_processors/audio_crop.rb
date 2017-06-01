7c61e4e475960a6f1988f45591b71ad06d064b7c
Stefan Harris <stfnhrrs@users.noreply.github.com>
2017-06-01 14:13:19 -0400

    Update audio_crop.rb

--- lib/paperclip_processors/audio_crop.rb
+++ lib/paperclip_processors/audio_crop.rb
@@ -15,9 +15,11 @@ module Paperclip
       dst = Tempfile.new([@basename, @current_format])
       dst.binmode
       begin
-        logger.error "FORMAT"
-        logger.error @current_format
-        success = Paperclip.run("ffmpeg -y -i #{src.path} -acodec copy -t #{@duration} -ss #{@offset} #{dst.path}")
+        if @current_format == "m4a"
+          success = Paperclip.run("ffmpeg -y -i #{src.path} -acodec libmp3lame -t #{@duration} -ss #{@offset} #{dst.path}")
+        else
+          success = Paperclip.run("ffmpeg -y -i #{src.path} -acodec copy -t #{@duration} -ss #{@offset} #{dst.path}")
+        end
       rescue PaperclipCommandLineError => e
         raise Paperclip::Error, "error while processing audio for #{@file}: #{e}"
       end
